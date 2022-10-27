37c72bd24cf781cead2931fccdc624f2c3d2db97
7pigy7 <zoo7654321@byui.edu>
2022-10-27 12:23:27 -0600

    discover most of

--- chamber/js/windChill.js
+++ chamber/js/windChill.js
@@ -1,1 +1,8 @@
 /** f = 35.74 + 0.6215t - 35.75s^0.16 + 0.4275ts^0.16 **/
+
+const temperature = document.getElementById(temp);
+const windSpeed = document.getElementById(speed);
+
+let windChill = 35.74 + 0.6215 * temperature - 35.75 * windSpeed ** 0.16 + 0.4275 * temperature * windSpeed ** 0.16;
+
+document.getElementById("chill").innerHTML = windChill;
