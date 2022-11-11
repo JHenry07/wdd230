797b791a83eff26e630cac4820cd8e731c3d8d35
7pigy7 <zoo7654321@byui.edu>
2022-11-11 12:46:26 -0800

    data

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -60,3 +60,53 @@ function getNumberOfDays(start, end) {
 }
 
 console.log(getNumberOfDays("2/1/2021", "3/1/2021"));
+
+
+const request = './json/data.json';
+const cards = document.querySelector('.cards');
+
+fetch(request)
+    .then(function (response) {
+        return response.json();
+    }) 
+    .then(function (jsonObject) {
+        // console.table(jsonObject); //Temporary checking for valid response and data parsing
+        const businesses = jsonObject['businesses'];
+        businesses.forEach(displayBusinesses);
+        // prophets.forEach(ordinalNumbers)
+    });
+
+function displayBusinesses(business){
+
+    let card = document.createElement('section')
+    let h2 = document.createElement('h2')
+    let parag = document.createElement('p')
+    let parag1 = document.createElement('p')
+    let portrait = document.createElement('img')
+
+    //change the textContent property of the h2 element to contain the 
+    //prophet's full name 
+    h2.textContent = `${business.name}`;
+
+    //change the textContent property of the p element to contain the 
+    //prophet's birth date
+    parag.textContent = `Website: ${business.website}`;
+
+    //change the textContent property of the p element to contain the 
+    //prophet's place pf birth
+    parag1.textContent = `Address: ${business.address}`;
+
+    // /Build the image attributes by using the setAttribute values. 
+    portrait.setAttribute('src', business.image);
+    portrait.setAttribute('alt', `Logo for ${business.name} - ${business.level} level`);
+    portrait.setAttribute('loading', 'lazy');
+
+    //Append the section card with the h2 element
+    card.appendChild(h2);
+    card.appendChild(parag);
+    card.appendChild(parag1);
+    card.appendChild(portrait);
+
+    //Append the existing html div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+};
