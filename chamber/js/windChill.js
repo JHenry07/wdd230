101fcadbb6dddad2205fb5928496e47d20700305
7pigy7 <zoo7654321@byui.edu>
2022-11-15 16:31:19 -0800

    ????

--- chamber/js/windChill.js
+++ chamber/js/windChill.js
@@ -1,10 +1,8 @@
 /** f = 35.74 + 0.6215t - 35.75s^0.16 + 0.4275ts^0.16 **/
 
-const temperature = document.getElementById(temp);
-const windSpeed = document.getElementById(speed);
+var temperature = document.getElementById(temp);
+var windSpeed = document.getElementById(speed);
 
-const windChill = 35.74 + 0.6215 * Number(temperature) - 35.75 * Number(windSpeed) ** 0.16 + 0.4275 * Number(temperature) * Number(windSpeed) ** 0.16;
+var windChill = 35.74 + 0.6215 * Number(temperature) - 35.75 * Number(windSpeed) ** 0.16 + 0.4275 * Number(temperature) * Number(windSpeed) ** 0.16;
 
-const chill = document.getElementById("chill");
-
-chill.innerHTML = windChill;
+document.getElementById("chill").innerHTML = windChill;
