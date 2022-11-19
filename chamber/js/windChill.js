391ffb5f3156248e08061d9b00c8a5dca5538001
7pigy7 <zoo7654321@byui.edu>
2022-11-19 15:58:38 -0800

    weather API

--- chamber/js/windChill.js
+++ chamber/js/windChill.js
@@ -1,6 +1,6 @@
 /** f = 35.74 + 0.6215t - 35.75s^0.16 + 0.4275ts^0.16 **/
 
-var temperature = document.getElementById(temp);
+var temperature = document.getElementById(currentTemp);
 var windSpeed = document.getElementById(speed);
 
 var windChill = 35.74 + 0.6215 * Number(temperature) - 35.75 * Number(windSpeed) ** 0.16 + 0.4275 * Number(temperature) * Number(windSpeed) ** 0.16;
