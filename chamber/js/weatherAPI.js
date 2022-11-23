5ed373b95d3a1b9fbd23c6aed34f67aa0a7c4ab0
7pigy7 <zoo7654321@byui.edu>
2022-11-23 11:52:06 -0800

    contact

--- chamber/js/weatherAPI.js
+++ chamber/js/weatherAPI.js
@@ -3,6 +3,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const currentWindSpeed = document.querySelector('#speed');
+const currentWindChill = document.querySelector('#chill');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=61.2181&lon=-149.9003&appid=80636e4fc7fc5e878a1c611a8f9b9281&units=imperial';
 
@@ -26,6 +27,7 @@ apiFetch();
 function displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
     currentWindSpeed.innerHTML = `${weatherData.wind.speed.toFixed(0)}`;
+    currentWindChill.innerHTML = `${weatherData.main.feels_like.toFixed(0)}`;
     
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
