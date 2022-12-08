e42bc672063096c0a77f0b898a0dc54a1d1e4f5e
7pigy7 <zoo7654321@byui.edu>
2022-12-07 16:19:24 -0800

    bf shell

--- bountifulFoods/javascript/scripts.js
+++ bountifulFoods/javascript/scripts.js
@@ -1,3 +1,13 @@
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+
+x.onclick = toggleMenu;
+
+
 const last = new Date(document.lastModified);
 document.querySelector('#last').textContent= last;
 const year = new Date();
